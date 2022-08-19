1e13cf32ed0952a5e1fd1e854966a8b4e3554977
jekovniki <jekovniki95@gmail.com>
2022-08-19 19:53:31 +0300

    Fetch-branches [=] refactor APIRequest [+] add header fail test to getRepository [=] minor refactor

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -5,7 +5,7 @@ import { handleErrors } from '../utils/helpers';
 
 dotenv.config();
 
-const token = process.env.PUBLIC_ACCESS_TOKEN ?? '';
+const token = process.env.PERSONAL_ACCESS_TOKEN ?? '';
 
 class APIRequest implements IFetch {
     private token: string;
@@ -16,13 +16,22 @@ class APIRequest implements IFetch {
 
     public async get(url: string, customHeader: Record<string, any> = {}): Promise<Record<any, string> | TErrorMessageResponse> {
         try {
+            if ('Accept' in customHeader && customHeader.Accept === 'application/xml') {
+                return handleErrors({
+                    response: {
+                        status: 406,
+                        statusText: 'Not Acceptable'
+                    },
+                    message: 'Request is not supported from the server'
+                })
+            }
             const requestInstance = axios.create({
                 headers: {
                     'Authorization': `${this.token}`,
                     ...customHeader
                 }
             });
-            const result = await requestInstance.get(`${url}`);
+            const result = await requestInstance.get(url);
 
             return result.data;
         } catch (error: any) {
--- test/get.test.ts
+++ test/get.test.ts
@@ -5,16 +5,22 @@ dotenv.config();
 
 const successfullRequest = {
     username: 'jekovniki',
-    repository: ''
+    repository: '',
+    header: {
+        Accept: 'application/json'
+    }
 }
 
 const failedRequest = {
-    username: 'nonExistingUser123'
+    username: 'nonExistingUser123',
+    header: {
+        Accept: 'application/xml'
+    }
 }
 
 describe('Controller', () => {
     test('+ getRepository | should return TGetRepositoryResponse', async () => {
-        const result: any= await getRepository(successfullRequest.username);
+        const result: any= await getRepository(successfullRequest.username, successfullRequest.header);
 
         successfullRequest.repository = result[0].repositoryName;
 
@@ -25,7 +31,15 @@ describe('Controller', () => {
         }
     });
 
-    test('- getRepository | should return TErrorMessageResponse', async () => {
+    test('- getRepository | should return TErrorMessageResponse: code 406', async () => {
+        const result: any = await getRepository(successfullRequest.username, failedRequest.header);
+
+        expect(result).toHaveProperty('status');
+        expect(result.status).toBe(406);
+        expect(result).toHaveProperty('Message');
+    });
+
+    test('- getRepository | should return TErrorMessageResponse: code 404', async () => {
         const result: any = await getRepository(failedRequest.username);
 
         expect(result).toHaveProperty('status');
